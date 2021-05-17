18f91180a0502c58a33da2a79a6ad614cf21b463
dcota <duartenunocota@gmail.com>
2021-05-17 17:09:50 +0000

    código aula

---
+++ GRSIArduinoControl/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace GRSIArduinoControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbCOM = new System.Windows.Forms.ComboBox();
+            this.btnObterCOMs = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.tbLEDRed = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTerminar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnONRed = new System.Windows.Forms.Button();
+            this.btnOFFRed = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbCOM
+            // 
+            this.cbCOM.FormattingEnabled = true;
+            this.cbCOM.Location = new System.Drawing.Point(61, 27);
+            this.cbCOM.Name = "cbCOM";
+            this.cbCOM.Size = new System.Drawing.Size(121, 23);
+            this.cbCOM.TabIndex = 0;
+            this.cbCOM.SelectedIndexChanged += new System.EventHandler(this.cbCOM_SelectedIndexChanged);
+            // 
+            // btnObterCOMs
+            // 
+            this.btnObterCOMs.Location = new System.Drawing.Point(73, 65);
+            this.btnObterCOMs.Name = "btnObterCOMs";
+            this.btnObterCOMs.Size = new System.Drawing.Size(98, 23);
+            this.btnObterCOMs.TabIndex = 1;
+            this.btnObterCOMs.Text = "Obter COMs";
+            this.btnObterCOMs.UseVisualStyleBackColor = true;
+            this.btnObterCOMs.Click += new System.EventHandler(this.btnObterCOMs_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(84, 117);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 2;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // tbLEDRed
+            // 
+            this.tbLEDRed.ForeColor = System.Drawing.SystemColors.Window;
+            this.tbLEDRed.Location = new System.Drawing.Point(61, 235);
+            this.tbLEDRed.Name = "tbLEDRed";
+            this.tbLEDRed.Size = new System.Drawing.Size(80, 23);
+            this.tbLEDRed.TabIndex = 3;
+            this.tbLEDRed.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "LED Vermelho";
+            // 
+            // btnTerminar
+            // 
+            this.btnTerminar.Location = new System.Drawing.Point(84, 157);
+            this.btnTerminar.Name = "btnTerminar";
+            this.btnTerminar.Size = new System.Drawing.Size(75, 23);
+            this.btnTerminar.TabIndex = 6;
+            this.btnTerminar.Text = "TERMINAR";
+            this.btnTerminar.UseVisualStyleBackColor = true;
+            this.btnTerminar.Click += new System.EventHandler(this.btnTerminar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(84, 386);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 7;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnONRed
+            // 
+            this.btnONRed.Location = new System.Drawing.Point(170, 235);
+            this.btnONRed.Name = "btnONRed";
+            this.btnONRed.Size = new System.Drawing.Size(75, 23);
+            this.btnONRed.TabIndex = 8;
+            this.btnONRed.Text = "ON";
+            this.btnONRed.UseVisualStyleBackColor = true;
+            this.btnONRed.Click += new System.EventHandler(this.btnONRed_Click);
+            // 
+            // btnOFFRed
+            // 
+            this.btnOFFRed.Location = new System.Drawing.Point(262, 235);
+            this.btnOFFRed.Name = "btnOFFRed";
+            this.btnOFFRed.Size = new System.Drawing.Size(75, 23);
+            this.btnOFFRed.TabIndex = 9;
+            this.btnOFFRed.Text = "OFF";
+            this.btnOFFRed.UseVisualStyleBackColor = true;
+            this.btnOFFRed.Click += new System.EventHandler(this.btnOFFRed_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 450);
+            this.Controls.Add(this.btnOFFRed);
+            this.Controls.Add(this.btnONRed);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnTerminar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbLEDRed);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnObterCOMs);
+            this.Controls.Add(this.cbCOM);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbCOM;
+        private System.Windows.Forms.Button btnObterCOMs;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.TextBox tbLEDRed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTerminar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnONRed;
+        private System.Windows.Forms.Button btnOFFRed;
+    }
+}
+
---
+++ GRSIArduinoControl/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GRSIArduinoControl
+{
+    public partial class Form1 : Form
+    {
+        private string[] availableCOMs;
+        private string selectedCOM;
+        private SerialPort port = new SerialPort();
+        private int inData;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnObterCOMs_Click(object sender, EventArgs e)
+        {
+            bool flag = false;
+            do
+            {
+                try
+                {
+                    availableCOMs = SerialPort.GetPortNames();
+                    if (availableCOMs.Length == 0)
+                    {
+                        throw new FormException();
+                    }
+                    else
+                    {
+                        cbCOM.Items.Clear();
+                        cbCOM.Items.AddRange(availableCOMs);
+                        flag = true;
+                    }
+                    
+                }
+                catch (FormException)
+                {
+                    if(!showException("Não há COMs disponíveis! \n Tentar de novo?\n"))
+                    {
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        flag = true;
+                    }
+                }
+            } while (flag == false);
+        }
+
+        public Boolean showException(string message)
+        {
+            string title = "";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons, 
+                MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+            if(result == DialogResult.Yes)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void cbCOM_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = cbCOM.SelectedIndex;
+            Object selectedIndex = cbCOM.SelectedItem;
+            selectedCOM = selectedIndex.ToString();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            bool flag = false;
+            do
+            {
+                try
+                {
+                    if(selectedCOM == null)
+                    {
+                        throw new FormException();
+                    }
+                    else
+                    {
+                        connect();
+                        //subscrever os eventos de chegada de dados à porta COM
+                        port.DataReceived += port_SerialDataReceivedEvent;
+                        flag = true;
+                    }
+
+                }
+                catch (FormException)
+                {
+                    if (!showException("Tem de selecionar uma COM! \n Tentar de novo?\n"))
+                    {
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        flag = true;
+                    }
+                }
+            } while (flag == false);
+        }
+
+        public void connect()
+        {
+            port.PortName = selectedCOM;
+            port.BaudRate = 9600;
+            port.Parity = Parity.None;
+            port.DataBits = 8; 
+            port.Open();
+            btnTerminar.Enabled = true;
+            btnIniciar.Enabled = false;
+            port.Write("I");
+        }
+
+        public void port_SerialDataReceivedEvent (object sender, SerialDataReceivedEventArgs e)
+        {
+            bool flag = false;
+            do
+            {
+                try
+                {
+                    //converte para int o caracter que chega (0 ou 1)
+                    inData = Int16.Parse(port.ReadLine());
+                    this.Invoke(new EventHandler(displayData));
+                }
+                catch (OperationCanceledException)
+                {
+                    flag = true;
+                }
+            } while (flag == false);
+            
+        }
+
+        public void displayData(object sender, EventArgs e)
+        {
+            tbLEDRed.Clear();
+            if(inData == 0)
+            {
+                tbLEDRed.BackColor = Color.Red;
+                tbLEDRed.Text = "DESLIGADO";
+            }
+            if(inData == 1)
+            {
+                tbLEDRed.BackColor = Color.Green;
+                tbLEDRed.Text = "LIGADO";
+            }
+
+             
+        }
+
+        private void btnTerminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (port.IsOpen)
+                {
+                    //anular a subscrição do evento DataReceived
+                    port.DataReceived -= port_SerialDataReceivedEvent;
+                    port.Close();
+                    btnTerminar.Enabled = false;
+                    btnIniciar.Enabled = true;
+                }  
+            }
+            catch (FormException)
+            {
+
+            }
+            
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (port.IsOpen)
+                {
+                    port.Close();
+                    //anular a subscrição do evento DataReceived
+                    port.DataReceived -= port_SerialDataReceivedEvent;
+                    Application.Exit();
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+            catch (FormException)
+            {
+
+            }
+        }
+
+        private void btnONRed_Click(object sender, EventArgs e)
+        {
+            port.Write("L");
+        }
+
+        private void btnOFFRed_Click(object sender, EventArgs e)
+        {
+            port.Write("D");
+        }
+    }
+
+
+
+    //classe de exceções personalizadas
+    public class FormException : Exception
+    {
+        public FormException()
+        {
+            //construtor
+        }
+    }
+}
